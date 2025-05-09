6676ad3543086e202298cd2162873ca1995a8e8b
juanm <juanmartindvd@gmail.com>
2025-05-09 19:41:30 +0200

    reversion de cambios

--- Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
+++ Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
@@ -1,28 +1,11 @@
 package com.gotrip.Go_Trip.DTO;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
-    private String username;
-    private String email;
+
+    private String usernameOrEmail;
     private String password;
 
-    // Add getters and setters
-    public String getUsernameOrEmail() {
-        return username != null && !username.isEmpty() ? username : email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
