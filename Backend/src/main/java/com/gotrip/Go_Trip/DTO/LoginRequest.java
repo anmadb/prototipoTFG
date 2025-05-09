4a5bb3f4da2c1496c0b975ca2a2806190e3ed346
juanm <juanmartindvd@gmail.com>
2025-05-09 20:18:12 +0200

    correct LoginRecuest el codigo se quejaba de los geters sin identificar el @Data

--- Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
+++ Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
@@ -8,4 +8,14 @@ public class LoginRequest {
     private String usernameOrEmail;
     private String password;
 
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    
+
 }
