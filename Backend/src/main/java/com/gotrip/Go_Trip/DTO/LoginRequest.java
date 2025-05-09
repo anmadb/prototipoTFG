cc1711ef0cc03a5eb1962bdce1e0dca5b05ed7fb
juanm <juanmartindvd@gmail.com>
2025-05-09 17:58:11 +0200

    Actualizacion de login

--- Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
+++ Backend/src/main/java/com/gotrip/Go_Trip/DTO/LoginRequest.java
@@ -1,11 +1,28 @@
 package com.gotrip.Go_Trip.DTO;
 
-import lombok.Data;
-
-@Data
 public class LoginRequest {
-
-    private String usernameOrEmail;
+    private String username;
+    private String email;
     private String password;
 
+    // Add getters and setters
+    public String getUsernameOrEmail() {
+        return username != null && !username.isEmpty() ? username : email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
